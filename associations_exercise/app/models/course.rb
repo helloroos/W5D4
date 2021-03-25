58799581a6c27b776e8e257d5fd7df7a41b6b2f2
michelleroos <hello.michelleroos@gmail.com>
2021-03-25 15:36:00 -0400

    Finished associations_exercise

--- associations_exercise/app/models/course.rb
+++ associations_exercise/app/models/course.rb
@@ -13,7 +13,7 @@ class Course < ApplicationRecord
 
     has_many :enrollments,
         primary_key: :id,
-        foreign_key: :prereq_id,
+        foreign_key: :course_id,
         class_name: :Enrollment
 
     has_many :enrolled_students,
@@ -24,4 +24,10 @@ class Course < ApplicationRecord
         primary_key: :id,
         foreign_key: :prereq_id,
         class_name: :Course
+    
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
+
 end
